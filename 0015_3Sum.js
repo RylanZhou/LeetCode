57a59e8d70b05cbb9b58ce088d919fa660ba4f9d
RylanZhou <475777146@qq.com>
2019-05-12 00:32:16 +0800

    Completed problem 0016 and 0017, modified method used in 0015.

--- 0015_3Sum.js
+++ 0015_3Sum.js
@@ -19,10 +19,55 @@
  * @return {number[][]}
  */
 function threeSum(nums) {
-  const numsCopy = nums.sort();
-  for (let i = 0; i < numsCopy.length; i++) {
+  const result = [];
+  // No solution if the nums given contains less than 3 elements
+  if (nums.length < 3) return result;
 
+  // Sort nums to make it easier to operate
+  const numsCopy = nums.sort((obj1, obj2) => obj1 - obj2);
+
+  // Picking out 3 elements means the left pointer will only be able to get length - 2
+  for (let i = 0; i < numsCopy.length - 2; i++) {
+    if (numsCopy[i] > 0) {
+      // The smallest one is bigger than the target 0
+      break;
+    }
+    if (i > 0 && numsCopy[i] === numsCopy[i - 1]) {
+      // Get rid of duplicated solutions
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+    // j is the middle pointer, running from the left pointer to the right pointer
+    let j = i + 1;
+    // k is the right pointer, beginning from the very end
+    let k = numsCopy.length - 1;
+
+    while (j < k) {
+      // If the middle pointer hasn't meet the right pointer
+      const sum = numsCopy[i] + numsCopy[j] + numsCopy[k];
+      if (sum === 0) {
+        result.push([numsCopy[i], numsCopy[j], numsCopy[k]]);
+        // Get rid of duplicated answers
+        while (j < k && numsCopy[j] === numsCopy[j + 1]) {
+          j++;
+        }
+        while (j < k && numsCopy[k] === numsCopy[k - 1]) {
+          k--;
+        }
+        // Finally, j has to run to a new position and k has to change the position as well
+        // Otherwise, their is no exit for the main while
+        j++;
+        k--;
+      } else if (sum < 0) {
+        // The sum is too small, meaning j has to be larger
+        j++;
+      } else {
+        // The sum is too large, meaning k has to be smaller
+        k--;
+      }
+    }
   }
+  return result;
 }
 
 function main() {
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# LeetCode
+# LeetCode Accepted Submissions
